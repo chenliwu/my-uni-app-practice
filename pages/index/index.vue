2fa7f1f4cd04bc4b81a23c308e38e8904fc2e567
chenlw <chenlw@bytter.com>
2019-06-14 10:23:22 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="content">
+		<view>
+			<text class="title">{{ title }}</text>
+		</view>
+		<view><button type="primary" @click="gotoLogin()">跳转到登录页面</button></view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Flexbox布局练习'
+		};
+	},
+	onLoad() {},
+	methods: {
+		gotoLogin: function() {
+			uni.navigateTo({
+				url: '../login/login'
+			});
+		}
+	}
+};
+</script>
+
+<style>
+.content {
+	flex: 1;
+	flex-direction: 'column';
+	justify-content: 'center';
+	align-items: 'center';
+}
+
+.title {
+	font-size: 20upx;
+	color: #8f8f94;
+}
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="uni-common-mt">
+		<view class="uni-form-item uni-column">
+			<view class="title">account:</view>
+			<input class="uni-input" v-model="account" placeholder="请输入账号" />
+		</view>
+		<view>
+			  <button type="primary" plain="true" @click="login()">登录</button>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			account:'',
+		};
+	},
+	methods: {
+		login:function(){
+			uni.showToast({
+				title:`账号:${this.account}`,
+				icon:'none'
+			});
+		}
+	}
+};
+</script>
+
+<style></style>
