5aa9efbf96c13f90c600065e7b6b9be43cbf5994
BYTTER\chenlw <chenlw@bytter.com>
2019-07-09 10:30:49 +0800

    modal组件运用

--- App.vue
+++ App.vue
@@ -1,28 +1,50 @@
 <script>
-export default {
-	onLaunch: function() {
-		console.log('App Launch');
-	},
-	onShow: function() {
-		console.log('App Show');
-	},
-	onHide: function() {
-		console.log('App Hide');
+	export default {
+		onLaunch: function () {
+			console.log('App Launch')
+		},
+		onShow: function () {
+			console.log('App Show')
+		},
+		onHide: function () {
+			console.log('App Hide')
+		}
 	}
-};
 </script>
 
 <style>
-/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
-@import './common/uni.css';
-
-@import './common/my_common.css';
-
-/*每个页面公共css */
-page {
-	background-color: #f4f5f6;
-	/* height: 100%; */
-	/* font-size: 28upx; */
-	/* line-height: 1.8; */
-}
+    page {
+    	display: flex;
+    	flex-direction: column;
+    	box-sizing: border-box;
+    	background-color: #FFFFFF;
+    }
+    .example{
+        padding: 0 30upx 30upx;
+    }
+    .example-title {
+    	font-size: 32upx;
+        line-height: 32upx;
+    	color: #666;
+    	margin: 40upx 25upx;
+    	padding-left: 25upx;
+        position: relative;
+    }
+    .example .example-title{
+        margin: 40upx 0;
+    }
+    .example-title:before {
+    	content: "";
+    	display: inline-block;
+    	position: absolute;
+    	left: 0upx;
+        top: 0upx;
+    	width: 2px;
+    	height: 34upx;
+    	background-color: #007aff;
+    	border-radius: 2px;
+    }
+    .example-body{
+        padding: 0 40upx;
+    }
 </style>
---
+++ pages/testUserAgent/testUserAgent.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<text></text>
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad() {
+			console.log('onLoad');
+			console.log('Window:',Window);
+			console.log('document:',document);
+			console.log('navigator.userAgent:',navigator.userAgent);
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
